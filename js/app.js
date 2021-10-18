c9b4dbb5b8b6d42f7c5b9441b56f6f42d9e58426
Wilfredo Palma <wilfre90dev@gmail.com>
2021-10-18 14:51:14 -0400

    Project Started

---
+++ js/app.js
@@ -0,0 +1,13 @@
+const requestURL = 'https://mdn.github.io/learning-area/javascript/oojs/json/superheroes.json';
+const request=new XMLHttpRequest();
+
+request.open('GET', requestURL)
+request.responseType = 'json';
+request.send();
+
+request.onload = function() {
+    const superHeroes = request.response; 
+    document.write(JSON.stringify(superHeroes))
+  }
+
+
