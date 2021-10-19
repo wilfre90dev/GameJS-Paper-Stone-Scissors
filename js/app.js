8eaa22ec4598ef8caa55f84465c4d7b031cc1100
Wilfredo Palma <wilfre90dev@gmail.com>
2021-10-19 14:17:37 -0400

    Template full, lets continue with JS

--- js/app.js
+++ js/app.js
@@ -1,13 +1,2 @@
-const requestURL = 'https://mdn.github.io/learning-area/javascript/oojs/json/superheroes.json';
-const request=new XMLHttpRequest();
-
-request.open('GET', requestURL)
-request.responseType = 'json';
-request.send();
-
-request.onload = function() {
-    const superHeroes = request.response; 
-    document.write(JSON.stringify(superHeroes))
-  }
 
 
